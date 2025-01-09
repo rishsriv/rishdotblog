b96148633d2adab304cc3c87d013933e02a247b0
Rishabh Srivastava <rishabhsriv@gmail.com>
2025-01-10 02:22:59 +0800

    add getNotesBySlug method

--- app/utils/mdx.server.ts
+++ app/utils/mdx.server.ts
@@ -1,6 +1,11 @@
-
 export const getEssayBySlug = (slug: string) : { default: () => any, frontmatter: Object} => {
   const essays = import.meta.glob("../content/essays/*.mdx", { eager: true });
   // @ts-ignore
   return essays[`../content/essays/${slug}.mdx`];
+};
+
+export const getNotesBySlug = (slug: string) : { default: () => any, frontmatter: Object} => {
+  const notes = import.meta.glob("../content/notes/*.mdx", { eager: true });
+  // @ts-ignore
+  return notes[`../content/notes/${slug}.mdx`];
 };
