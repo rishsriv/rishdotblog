c4634c92ffb5294c78e1fd100349b4326bb86d06
Rishabh Srivastava <rishabhsriv@gmail.com>
2025-01-10 02:56:30 +0800

    pre-rendering for dynamic slugs

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,10 +1,23 @@
 import type { Config } from "@react-router/dev/config";
-import { prerender } from "react-dom/static";
 
 export default {
   // Config options...
   // Use prerendering for every page, for now
   // this might get tedious in the future, if we have a lot of pages
   // at that point, we can consider using server side rendering
-  prerender: true,
+  // prerender: true,
+
+  async prerender() {
+    const essays = await import.meta.glob("./app/content/essays/*.mdx");
+    const notes = await import.meta.glob("./app/content/notes/*.mdx");
+    
+    // get all the dynamic slugs explicitly listed here
+    const essaySlugs = Object.keys(essays).map((path: string) => path.replace("app/content/", "/").replace(".mdx", ""));
+    const noteSlugs = Object.keys(notes).map((path: string) => path.replace("app/content/", "/").replace(".mdx", ""));
+
+    return ["/", "/contact", "/essays", "/notes"].concat(
+      essaySlugs,
+      noteSlugs
+    );
+  },
 } satisfies Config;
